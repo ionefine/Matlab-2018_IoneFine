44596c1f55b494a699c2ea815ff9667f9ca8d78a
Ione Fine <ionefine@uw.edu>
2018-01-03 12:24:04 -0800

    Add files via upload

---
+++ BreakfastCereal.m
@@ -0,0 +1,15 @@
+% BreakfastCereal
+%
+% Provides examples of fun things you can do with cereal boxes.
+% Inspired by my husbands mis-spelt childhood
+%
+% written IF 1/3/2018 for Matlab 2018
+
+frosted = 'FROSTED FLAKES';
+cheerios = 'CHEERIOS';
+
+frosted_scrambled = frosted;
+
+frosted_scrambled(9)='E';
+
+disp(frosted_scrambled);
